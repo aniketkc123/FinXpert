b472576ed333f2e769678db7184ec95b5ad2bcd1
ayushku634 <ayushiit634@gmail.com>
2025-03-28 16:32:00 +0530

    Frontend issue updated

--- frontend/src/scenes/landingPage/index.jsx
+++ frontend/src/scenes/landingPage/index.jsx
@@ -5,7 +5,7 @@ import HeroSection from "../../components/landingPage/HeroSection";
 import FeaturesSection from "../../components/landingPage/FeaturesSection";
 import TechnicalAchievements from "../../components/landingPage/TechnicalAchievements";
 import CallToActionSection from "../../components/landingPage/CallToActionSection";
-import TeamSection from "../../components/landingPage/TeamSection";
+// import TeamSection from "../../components/landingPage/TeamSection";
 import Footer from "../../components/landingPage/Footer";
 import NotesSection from "../../components/landingPage/NotesSection";
 
@@ -17,7 +17,7 @@ const LandingPage = () => {
 			<NotesSection />
 			<FeaturesSection />
 			<TechnicalAchievements />
-			<TeamSection />
+			{/* <TeamSection /> */}
 			<CallToActionSection />
 			<Footer />
 		</>
